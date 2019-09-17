b8040a4b73917a16d43528da94506bbdd6eac139
Hsin-Hung Chen <cxh@ms1.url.com.tw>
2019-09-17 23:35:46 +0800

    Initial Commit

---
+++ 20190917_HideKeyboard_tutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  20190917_HideKeyboard_tutorial
+//
+//  Created by chen hsin hung on 2019/9/17.
+//  Copyright © 2019 chen hsin hung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
